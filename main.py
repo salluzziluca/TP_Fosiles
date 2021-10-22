eae7db7d68ecffebc1343de874950b5743973760
Luca Salluzzi <salluzzi.luca@gmail.com>
2021-10-22 11:47:42 -0300

    Probando mostrar fichas

--- main.py
+++ main.py
@@ -5,18 +5,21 @@ def generar_fichas():
     return [["D",False],["D",False],["s",False],["s",False]]
 
 def mostrar_las_fichas(fichas):
-    #reci
+    for ficha in fichas:
+        if ficha[1]:
+            print('[{}]'.format(ficha[0]))
     pass
 
 def input_usuario():
     pass
 
 def main():
-    no_parar=True
+   # no_parar=True
     fichas=generar_fichas()
     mostrar_las_fichas(fichas) 
-    while no_parar:
-        fichas=input_usuario()
-        fichas=mostrar_las_fichas()
-        no_parar=ganasteinterrogacion()
+    #while no_parar:
+      #  fichas=input_usuario()
+      #  fichas=mostrar_las_fichas()
+       # no_parar=ganasteinterrogacion()
+main()
 
