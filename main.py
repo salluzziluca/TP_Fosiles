f25d09f2d6103d266f243ce692d6b30e77d86b7b
Luca Salluzzi <salluzzi.luca@gmail.com>
2021-11-02 10:55:38 -0300

    volví al commit de antes para dejar limpio el main

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random, time
-from tkinter import Button, Entry, Frame, Label, Tk
 
 POSICION_BOOL=1
 POSICION_LETRA = 0
@@ -138,33 +137,3 @@ def main():
             
 
 main()
-
-from tkinter import*
-
-def solicitar_nombre():
-    raiz= Tk()
-    raiz.title("Fosiles Memotest")
-    raiz.resizable(0,0)
-#raiz.iconbitmap()
-    raiz.geometry("300x150")
-    raiz.config(bg="yellow")
-#frame
-    miFrame=Frame(raiz)
-    miFrame.pack(padx=10, pady=20)
-    miFrame.config(cursor="heart")
-#jugadores j1
-    jugador_1=Label(miFrame, text="Primer Jugador: ")
-    jugador_1.grid(row=0,column=0, padx=10, pady=10)
-    jugador_1_entry=Entry(miFrame)
-    jugador_1_entry.grid(row=0,column=1,padx=10, pady=10)
-#j2
-    jugador_2=Label(miFrame, text="Segundo Jugador: ")
-    jugador_2.grid(row=1,column=0,padx=10, pady=10)
-    jugador_2_entry=Entry(miFrame)
-    jugador_2_entry.grid(row=1,column=1,padx=10, pady=10)
-#Boton
-    Boton=Button(raiz, text="Enviar",command = lambda: acierto(jugador_1_entry.get(),jugador_2_entry.get()))
-    Boton.pack()
-    raiz.mainloop()
-    solicitar_nombre()
-
