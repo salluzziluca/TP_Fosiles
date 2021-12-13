60760b9d411b801afc4fd1fed6c6fc0ce15c0494
Luca Salluzzi <salluzzi.luca@gmail.com>
2021-12-13 08:39:07 -0300

    Movi de main a etapa 8

--- Interfaz.py
+++ Interfaz.py
@@ -22,19 +22,27 @@ def validaciones(usuario, contraseña):
     return valido
 
 def presionar_enviar(dict_jugadores, usuario, contraseña, listbox_jugadores, mensaje_login):
-        # Valentina Nieto,Oriz Omar, Luca Salluzzi,Agustín Conti,Lucas Osorio.
-        #Se ejecuta al presionar el Boton.Si el usuario y la contraseña coinciden con los registros, asigna el contenido de los entry al diccionario de jugadores y a la listbox presente en memoria, ademas notifica al usuario en ambos casos.
+    # Valentina Nieto,Oriz Omar, Luca Salluzzi,Agustín Conti,Lucas Osorio.
+    #Se ejecuta al presionar el Boton.Si el usuario y la contraseña coinciden con los registros, asigna el contenido de los entry al diccionario de jugadores y a la listbox presente en memoria, ademas notifica al usuario en ambos casos.
     
-        if validaciones(usuario, contraseña):
-            dict_jugadores[usuario] = [0,0]
-            mensaje_login.config(bg = 'yellow',fg = 'black',text='Usuario ingresado correctamente')
-            listbox_jugadores.insert(END, usuario)
-                
-        else:
-            mensaje_login.config(bg = 'yellow',fg = 'black',text = 'Usuario y contraseña no coinciden con nuestros registros')
-                
-        return None
-    
+    if validaciones(usuario, contraseña):
+        dict_jugadores[usuario] = [0,0]
+        mensaje_login.config(bg = 'yellow',fg = 'black',text='Usuario ingresado correctamente')
+        listbox_jugadores.insert(END, usuario)
+            
+    else:
+        mensaje_login.config(bg = 'yellow',fg = 'black',text = 'Usuario y contraseña no coinciden con nuestros registros')
+            
+    return None
+
+def verificar_cantidad_jugadores(dict_jugadores, boton_envio, boton_inicio):
+    #Luca Salluzzi
+    #Verifica si la cantidad de usuarios logeados (listos para jugar) es igual o mayor a la constante del archivo de configuración. 
+    if len(dict_jugadores.keys()) >= (MAX_JUGADORES):
+        boton_envio['state']='disabled'
+    if len(dict_jugadores.keys()) >= MINIMO_JUGADORES:
+        boton_inicio['state'] = 'active'
+        
 def solicitar_nombre(dict_jugadores):
     #Hecho por Valentina Nieto y Camila Zarza, Oriz Omar, Luca Salluzzi, Agustín Conti, Lucas Osorio.
     #Solicita el ingreso de los nombres de los Jugadores, muestra por pantalla el boton de registro, el de inicio de juego, el maximo de jugadores posibles, los nombres de los jugadores logueados y ademas, mensajes por pantalla del estado del login.
@@ -80,17 +88,8 @@ def solicitar_nombre(dict_jugadores):
     listbox_jugadores.pack()
     
     #Funcion deshabilitación del botón
-    def verificar_cantidad_jugadores():
-        #Luca Salluzzi
-        #Verifica si la cantidad de usuarios logeados (listos para jugar) es igual o mayor a la constante del archivo de configuración. 
-        if len(dict_jugadores.keys()) >= (MAX_JUGADORES):
-            boton_envio['state']='disabled'
-        if len(dict_jugadores.keys()) >= MINIMO_JUGADORES:
-            boton_inicio['state'] = 'active'
+   
             
-    #Boton Envio
-    boton_envio=Button(raiz, text = "Logearse",command= lambda:[presionar_enviar(dict_jugadores, usuario_var.get(), contraseña_var.get(), listbox_jugadores, mensaje_login), verificar_cantidad_jugadores()])
-    boton_envio.pack()
     
     # Boton Registro
     boton_registro=Button(raiz, text ="Registrarse",command = lambda: interfaz_registro(raiz))
@@ -98,5 +97,10 @@ def solicitar_nombre(dict_jugadores):
     #Boton Iniciar
     boton_inicio=Button(raiz, text = "Iniciar Partida", state = DISABLED,command = raiz.destroy)
     boton_inicio.pack()
+
+    #Boton Envio
+    boton_envio=Button(raiz, text = "Logearse")
+    boton_envio.configure(lambda:[presionar_enviar(dict_jugadores, usuario_var.get(), contraseña_var.get(), listbox_jugadores, mensaje_login), verificar_cantidad_jugadores(dict_jugadores, boton_envio, boton_inicio)])
+    boton_envio.pack()
     raiz.mainloop()
     return None
