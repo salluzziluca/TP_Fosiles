e00b13703ea0423785e9ebe30032c3aaceeb9e0a
carpincha <vnieto@fi.uba.ar>
2021-12-13 21:52:37 -0300

    Cambios a la interfaz de registro: implementado boton registro nuevos colores de la interfaz y botones, nueva fuente y ya no se definen funciones dentro del codigo de la interfaz.

--- interfaz_de_registro.py
+++ interfaz_de_registro.py
@@ -73,6 +73,37 @@ def pass_valida(contrasenia):
 
     return valido
 
+def interaccion_usuario(validacion,msg_final,usario_nuevo_entry,mi_clave_entry,mi_clave_re_entry):
+    # recibe la tupla con los resultados de las validaciones y se comunica con el usuario de manera acorde.
+    if validacion[0] and validacion[1] and validacion[2]:
+        msg_final.config(bg='#d4e6f1',fg='green',text='Registro exitoso!')
+        msg_final.pack(padx= 70, pady=0)
+        # si el registro es exitoso se limpian los campos.
+        usario_nuevo_entry.delete(0, 'end')
+        mi_clave_entry.delete(0, 'end')
+        mi_clave_re_entry.delete(0, 'end')
+    elif not validacion[0]:
+        msg_final.config(bg='#d4e6f1',fg='red',text='Nombre de usuario inválido.\nPuede contener letras y numeros y el _.\nDebe tener entre 4 y 15 caracteres.\nNo puede ser un nombre de usuario existente.')
+        msg_final.pack(padx= 70, pady=0)
+    elif not validacion[1]:
+        msg_final.config(bg='#d4e6f1',fg='red',text='Clave no valida.\ndebe tener al menos una mayuscula, una minuscula, un numero, - y _\nDebe tener entre 8 y 12 caracteres.')
+        msg_final.pack(padx= 70, pady=0)
+    elif not validacion[2]:
+        msg_final.config(bg='#d4e6f1',fg='red',text='Las contraseñas no son iguales.')
+        msg_final.pack(padx= 70, pady=0)
+    return None
+def get_usuario_contra(var_usuario_nuevo,var_pass,var_pass_re):
+    #obtiene los datos ingresados en los campos de la interfaz al momento de la llamada de esta función.
+    return (var_usuario_nuevo.get(),var_pass.get(),var_pass_re.get())
+
+def presionar_boton_registrarse(var_usuario_nuevo,var_pass,var_pass_re,msg_final,usario_nuevo_entry,mi_clave_entry,mi_clave_re_entry):
+    # organiza la secuencia de desencadenadores al apretar el botón 'Registrarse'.
+    usuario_nuevo,contrasenia,contrasenia_re=get_usuario_contra(var_usuario_nuevo,var_pass,var_pass_re)
+    es_valido = (jugador_valido(usuario_nuevo), pass_valida(contrasenia),(contrasenia == contrasenia_re)) # tupla con tres booleanos validadores.
+    interaccion_usuario(es_valido,msg_final,usario_nuevo_entry,mi_clave_entry,mi_clave_re_entry)
+    if es_valido[0] and es_valido[1] and es_valido[2]:
+        guardar_usuario_nuevo(usuario_nuevo,contrasenia) # si es válido se guarda en el archivo de usuarios.
+    return None
 
 ########################################
 #################  Front #######################
@@ -83,11 +114,11 @@ def interfaz_registro(raiz_importada):
     raiz_registro.resizable(0,0)
     raiz_registro.geometry("515x315")
     raiz_registro.iconbitmap("fosil.ico")
-    raiz_registro.config(bg="green")
+    raiz_registro.config(bg="#d4e6f1")
     #---------------------------------- frame--------------------------------------------
-    frame_registro=Frame(raiz_registro)
+    frame_registro=Frame(raiz_registro,bg="#eaf2f8")
     frame_registro.pack(padx= 10, pady=20)
-
+    
     frame_registro.rowconfigure(0,pad=10)
     frame_registro.rowconfigure(2,minsize = 80)
     frame_registro.rowconfigure(4,minsize=40)
@@ -95,69 +126,43 @@ def interfaz_registro(raiz_importada):
     frame_registro.columnconfigure(3,minsize = 10)
 
     #---------------------------------- usuario--------------------------------------------
-    usario_nuevo=Label(frame_registro, text = "Nombre del Jugador: ",font=("Bahnschrift", 15))
+    usario_nuevo=Label(frame_registro, text = "Nombre del Jugador: ",font=("Lucida console", 11),bg="#eaf2f8")
     usario_nuevo.grid(row=0,column=0)
     var_usuario_nuevo=StringVar()
-    usario_nuevo_entry=Entry(frame_registro, textvariable = var_usuario_nuevo,font=("Bahnschrift", 15))
+    usario_nuevo_entry=Entry(frame_registro, textvariable = var_usuario_nuevo,font=("Lucida console", 11),bg="#eaf2f8",fg= "#273746",cursor="heart")
     usario_nuevo_entry.grid(row=0,column=1)
     #---------------------------------- contraseña--------------------------------------------
-    mi_clave=Label(frame_registro, text="Clave: ",font=("Bahnschrift", 15))
+    mi_clave=Label(frame_registro, text="Clave: ",font=("Lucida console", 11),bg="#eaf2f8")
     mi_clave.grid(row=2,column=0)
     var_pass=StringVar()
-    mi_clave_entry=Entry(frame_registro, textvariable=var_pass,font=("Bahnschrift", 15))
+    mi_clave_entry=Entry(frame_registro, textvariable=var_pass,font=("Lucida console", 11),bg="#eaf2f8",fg= "#273746",cursor="heart")
     mi_clave_entry.config(show="*")
     # para implementar el boton ojito. mi_clave_entry.config(show= str)
     mi_clave_entry.grid(row=2,column=1)
     #---------------------------------- REINGRESAR contraseña--------------------------------------------
-    mi_clave_re=Label(frame_registro, text="Reingresar Clave: ",font=("Bahnschrift", 15))
+    mi_clave_re=Label(frame_registro, text="Reingresar Clave: ",font=("Lucida console", 11),bg="#eaf2f8")
     mi_clave_re.grid(row=4,column=0)
     var_pass_re=StringVar()
-    mi_clave_re_entry=Entry(frame_registro, textvariable = var_pass_re,font=("Bahnschrift", 15))
+    mi_clave_re_entry=Entry(frame_registro, textvariable = var_pass_re,font=("Lucida console", 11),bg="#eaf2f8",fg= "#273746",cursor="heart")
     mi_clave_re_entry.config(show="*")
     mi_clave_re_entry.grid(row=4,column=1)
     #---------------------------------- Label dador de resultado--------------------------------------------
     msg_final=Label(raiz_registro, text='')
-    msg_final.config(bg='green',fg='white')
+    msg_final.config(bg='#d4e6f1',fg='white')
     msg_final.pack(padx= 70, pady=0)
     #---------------------------------- interaccion con usuario.--------------------------------------------
-    def interaccion_usuario(validacion):
-        # recibe la tupla con los resultados de las validaciones y se comunica con el usuario de manera acorde.
-        if validacion[0] and validacion[1] and validacion[2]:
-            msg_final.config(bg='green',fg='white',text='Registro exitoso!')
-            msg_final.pack(padx= 70, pady=0)
-            # si el registro es exitoso se limpian los campos.
-            usario_nuevo_entry.delete(0, 'end')
-            mi_clave_entry.delete(0, 'end')
-            mi_clave_re_entry.delete(0, 'end')
-        elif not validacion[0]:
-            msg_final.config(bg='green',fg='white',text='Nombre de usuario inválido.\nPuede contener letras y numeros y el _.\nDebe tener entre 4 y 15 caracteres.\nNo puede ser un nombre de usuario existente.')
-            msg_final.pack(padx= 70, pady=0)
-        elif not validacion[1]:
-            msg_final.config(bg='green',fg='white',text='Clave no valida.\ndebe tener al menos una mayuscula, una minuscula, un numero, - y _\nDebe tener entre 8 y 12 caracteres.')
-            msg_final.pack(padx= 70, pady=0)
-        elif not validacion[2]:
-            msg_final.config(bg='green',fg='white',text='Las contraseñas no son iguales.')
-            msg_final.pack(padx= 70, pady=0)
-        return None
+
 
     #---------------------------------- funciones--------------------------------------------
-    def get_usuario_contra():
-        #obtiene los datos ingresados en los campos de la interfaz al momento de la llamada de esta función.
-        return (var_usuario_nuevo.get(),var_pass.get(),var_pass_re.get())
 
-    def presionar_boton_registrarse():
-        # organiza la secuencia de desencadenadores al apretar el botón 'Registrarse'.
-        usuario_nuevo,contrasenia,contrasenia_re=get_usuario_contra()
-        print(usuario_nuevo,contrasenia,contrasenia_re)
-        es_valido = (jugador_valido(usuario_nuevo), pass_valida(contrasenia),(contrasenia == contrasenia_re)) # tupla con tres booleanos validadores.
-        interaccion_usuario(es_valido)
-        if es_valido[0] and es_valido[1] and es_valido[2]:
-            guardar_usuario_nuevo(usuario_nuevo,contrasenia) # si es válido se guarda en el archivo de usuarios.
-        return None
-    #---------------------------------- botón REGISTRAR--------------------------------------------
-    
-    boton_de_registro = Button(raiz_registro,text= "Registrarse",font=("Bahnschrift", 11), command = presionar_boton_registrarse )
-    boton_de_registro.pack(anchor = S)
+    #---------------------------------- botón REGISTRAR/CERRAR--------------------------------------------
+    frame_de_boton=Frame(raiz_registro,bg="#d4e6f1")
+    frame_de_boton.pack(anchor=S)
+    boton_de_registro = Button(frame_de_boton,text= "Registrarse",font=("Lucida console", 11), command = lambda: presionar_boton_registrarse(var_usuario_nuevo,var_pass,var_pass_re,msg_final,usario_nuevo_entry,mi_clave_entry,mi_clave_re_entry),bg="#eafaf1",activebackground="#a2d9ce")
+    boton_de_registro.grid(column = 3,row = 0,padx=60)
+    boton_de_cerrar = Button(frame_de_boton,text= "Cerrar",font=("Lucida console", 11), command = raiz_registro.destroy,bg="#e8daef",activebackground="#d2b4de")
+    boton_de_cerrar.grid(column = 0,row = 0,padx=60)
+
 
     raiz_registro.mainloop()
     return None
