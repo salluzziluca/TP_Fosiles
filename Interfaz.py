e22396ec1aa906763b7bfac50c0e5843950c53a5
0marO <92110097+0marO@users.noreply.github.com>
2021-12-14 16:02:52 -0300

    Cambio de fuente en la interfaz. Estructura de la interfaz de login Emprolijada.

--- Interfaz.py
+++ Interfaz.py
@@ -31,9 +31,9 @@ def presionar_enviar(dict_jugadores, usuario, contraseña, listbox_jugadores, me
             mensaje_login.config(bg = '#E9F7EF',fg = 'black',text='Usuario ingresado correctamente')
             listbox_jugadores.insert(END, usuario)
         elif (usuario in dict_jugadores.keys()):
-            mensaje_login.config(bg = '#E9F7EF',fg = 'black',text='El Usuario ya se encuentra logeado')
+            mensaje_login.config(bg = '#E9F7EF',fg = 'black',text='El Usuario ya se encuentra\nlogeado')
         else:
-            mensaje_login.config(bg = '#E9F7EF',fg = 'black',text = 'Usuario y contraseña \nno coinciden con nuestros registros')
+            mensaje_login.config(bg = '#E9F7EF',fg = 'black',text = 'Usuario y contraseña \nno coinciden con nuestros \nregistros')
                 
         return None
 
@@ -88,28 +88,29 @@ def solicitar_nombre(dict_jugadores):
     raiz.title("Fosiles Memotest")
     raiz.protocol("WM_DELETE_WINDOW", lambda: cerrar_juego_por_login(dict_jugadores,raiz))
     raiz.resizable(0,0)
-    raiz.geometry("415x400")
+    raiz.geometry("415x420")
     raiz.config(bg="#E9F7EF")
     raiz.iconbitmap('fosil.ico')
     
+    fuente_elegida = ('Lucida Console', 10)
     #frame
     miFrame=Frame(raiz)
-    miFrame.grid(in_=raiz, row=2, column=0, columnspan=3, sticky=NSEW, padx= 40)
+    miFrame.grid(in_=raiz, row=2, column=0, columnspan=3, sticky=NW, padx= 40)
 
     miFrame.config(cursor="heart", bg= '#E9F7EF')
    
     #casilla usario
-    usuario_existente=Label(miFrame, text="Usuario: ", bg= '#E9F7EF')
+    usuario_existente=Label(miFrame, text="Usuario: ", bg= '#E9F7EF',font=fuente_elegida)
     usuario_existente.grid(row=0,column=0, padx=0, pady=0)
     usuario_var = StringVar()
-    usuario_existente_entry=Entry(miFrame,textvariable=usuario_var)
+    usuario_existente_entry=Entry(miFrame,textvariable=usuario_var,font=fuente_elegida)
     usuario_existente_entry.grid(row=0,column=1,padx=10, pady=10)
     
     #casilla contraseña
-    contraseña=Label(miFrame, text="Contraseña: ", bg= '#E9F7EF')
-    contraseña.grid(row=1,column=0,padx=10, pady=10)
+    contraseña=Label(miFrame, text="Contraseña: ", bg= '#E9F7EF',font=fuente_elegida)
+    contraseña.grid(row=1,column=0,padx=0, pady=10)
     contraseña_var = StringVar()
-    contraseña_entry=Entry(miFrame,textvariable= contraseña_var)
+    contraseña_entry=Entry(miFrame,textvariable= contraseña_var,font=fuente_elegida)
     contraseña_entry.grid(row=1,column=1,padx=10, pady=10)
     contraseña_entry.config(show="*")
     
@@ -119,44 +120,55 @@ def solicitar_nombre(dict_jugadores):
     show_contra = Button(miFrame,image= ojo_abierto,command = lambda: presionar_ojo_abierto(show_contra,ojo_abierto,ojo_tachado,contraseña_entry),bg="#d1f2eb")
     show_contra.grid(row = 1, column = 2)
     
+    # Frame para mensajes.
+    frame_mensajes = Frame (raiz,bg= '#E9F7EF')
+    frame_mensajes.grid( padx= 50,sticky=NW)
     #Mensaje login
-    mensaje_login = Label(raiz, text = '', height=2, width=30)
-    mensaje_login.config(bg = '#E9F7EF',fg = 'black')
-    mensaje_login.grid(row=3, column=1, padx=10, pady=10)
+    mensaje_login = Label(frame_mensajes, text = '', height=2, width=30)
+    mensaje_login.config(bg = '#E9F7EF',fg = 'black',font=fuente_elegida)
+    mensaje_login.grid(row=0, column=1, padx=10, pady=10,ipady=10)
     
     #Mensaje max jugadores 
-    mensaje_jugadores = Label(raiz, text = f'El maximo total de jugadores es {MAX_JUGADORES}')
+    mensaje_jugadores = Label(frame_mensajes, text = f'El maximo total de jugadores es {MAX_JUGADORES}',font=fuente_elegida)
     mensaje_jugadores.config(bg = '#E9F7EF',fg = 'black')
-    mensaje_jugadores.grid(row=4, column=1, padx=10, pady=10)
+    mensaje_jugadores.grid(row=1, column=1, padx=10, pady=10)
     
+
+    #Frame para Listbox y boton desloguear.
+    frame_list_deslog = Frame (raiz,bg= '#E9F7EF')
+    frame_list_deslog.grid( padx= 63,sticky=NW)
     #Listbox jugadores
-    listbox_jugadores = Listbox(raiz)
+    listbox_jugadores = Listbox(frame_list_deslog)
     
     if dict_jugadores:   # Continuar logeados
         continuan_logeados(dict_jugadores,listbox_jugadores)
     listbox_jugadores.config(selectbackground="#E9F7EF",selectforeground="black",border= 2,font=('Lucida Console',10))
-    listbox_jugadores.grid(row=5, column=1, pady=10)
+    listbox_jugadores.grid(row=5, column=1, pady=10,padx=20)
     
+    
+    # Frame Botones Registro e iniciar.
+    Frame_botones = Frame(raiz,bg= '#E9F7EF')
+    Frame_botones.grid(padx=5,sticky=NW,pady=20)
     #Boton Envio
     longitud_diccionario = len(dict_jugadores) # Para control de estado de botones en partidas sucesivas.
-    boton_envio=Button(raiz, text = "Logearse")
+    boton_envio=Button(Frame_botones, text = "Logearse",font=fuente_elegida)
     boton_envio.config(command= lambda:[presionar_enviar(dict_jugadores, usuario_var.get(), contraseña_var.get(), listbox_jugadores, mensaje_login), verificar_cantidad_jugadores(dict_jugadores, boton_envio, boton_inicio)])
     if longitud_diccionario  >= MAX_JUGADORES:  # Para control de estado de botones en partidas sucesivas.
         boton_envio.config(state= DISABLED)
-    boton_envio.grid(row=6, column=0, padx=10, pady=10)
+    boton_envio.grid(row=6, column=0, padx=5, pady=10,sticky=W)
     
     # Boton Registro
-    boton_registro=Button(raiz, text ="Registrarse",command = lambda: interfaz_registro(raiz))
-    boton_registro.grid(row=6, column=1, padx=10, pady=10)
+    boton_registro=Button(Frame_botones, text ="Registrarse",command = lambda: interfaz_registro(raiz),font=fuente_elegida)
+    boton_registro.grid(row=6, column=1, padx=40, pady=10,sticky=S)
     
     #Boton Iniciar
-    boton_inicio=Button(raiz, text = "Iniciar Partida", state = DISABLED,command = raiz.destroy)
+    boton_inicio=Button(Frame_botones, text = "Iniciar Partida", state = DISABLED,command = raiz.destroy,font=fuente_elegida)
     if longitud_diccionario >= MAX_JUGADORES:   # Para control de estado de botones en partidas sucesivas.
         boton_inicio.config(state= NORMAL)
-    boton_inicio.grid(row=6, column=2, padx=10, pady=10)
+    boton_inicio.grid(row=6, column=2, padx=5, pady=10,sticky=E)
 
     #Boton Desloguear
-    boton_desloguear = Button(raiz,text= "Desloguear\nJugador")
+    boton_desloguear = Button(frame_list_deslog,text= "Desloguear\nJugador",font=fuente_elegida)
     boton_desloguear.config(command= lambda: presionar_desloguear(dict_jugadores,listbox_jugadores,boton_envio,boton_inicio) )
     boton_desloguear.grid(row=5, column=2, pady=10)
 
