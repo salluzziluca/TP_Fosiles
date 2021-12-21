f082a42487bfb1389504df584fe1117909252be9
0marO <92110097+0marO@users.noreply.github.com>
2021-12-21 20:38:37 -0300

    Cambio de prueba

--- config/Constantes_config.py
+++ config/Constantes_config.py
@@ -8,7 +8,7 @@ def leer_config():
     while linea:
         if linea[1].isnumeric():
             variables.append(int(linea[1]))
-        elif linea[1] == "False" :
+        elif linea[1] == "False":
             variables.append(False)
         elif linea[1] == "True":
             variables.append(True)
@@ -21,24 +21,21 @@ def leer_config():
 # ARCHIVO DE CONFIG 
 CANTIDAD_FICHAS, MAX_JUGADORES, MAX_PARTIDAS, REINICIAR_ARCHIVO_PARTIDAS = leer_config()
 
-
-
+# POSICIONES
 POSICION_LETRA = 0
-POSICION_BOOL=1
+POSICION_BOOL = 1
 INGRESO1 = 0
 INGRESO2 = 1
-
 
 # CONSTANTES DE INICIALIZACION
 INICIAR_CANT_PARTIDAS = 1
 
-
 # CONSTANTES DEL CSV:
+POS_NOMBRE_REGISTRO = 2
 POS_ACIERTOS_REGISTRO = 3
 POS_INTENTOS_REGISTRO = 4
-POS_NOMBRE_REGISTRO = 2
 
 # CONSTANTES DE POSICION DE DICCIONARIO
-INTENTOS= 0
-ACIERTOS=1
+INTENTOS = 0
+ACIERTOS = 1
 PARTIDAS_JUGADAS = 2
